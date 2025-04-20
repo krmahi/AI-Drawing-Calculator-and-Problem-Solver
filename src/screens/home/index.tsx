432a5e4011fe58ebeb34c6980e9946cbcb0daf0f
krmahi <mahesh.kr.2277@gmail.com>
2025-04-20 21:01:01 +0530

    add-package: React Draggable for latex dragging | latex implementation

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { ColorSwatch, Group } from "@mantine/core";
 import { Button } from "@/components/ui/button";
+import Draggable from "react-draggable";
 import axios from "axios";
 
 import { SWATCHES } from "@/../constants";
@@ -22,14 +23,33 @@ export default function Home() {
   const [color, setColor] = useState("rgb(255, 255, 255)");
   const [reset, setReset] = useState(false);
   const [result, setResult] = useState<GenerateResult>();
+  const [latexExpression, setLatexExpression] = useState<Array<string>>([]);
+  const [latexPosition, setLatexPosition] = useState({ x: 10, y: 200 });
   const [dictOfVars, setDictOfVars] = useState({});
 
   useEffect(() => {
     if (reset) {
       resetCanvas();
+      setLatexExpression([]);
+      setResult(undefined);
+      setDictOfVars({});
       setReset(false);
     }
   }, [reset]);
+
+  useEffect(() => {
+    if (latexExpression.length > 0 && window.MathJax) {
+      setTimeout(() => {
+        window.MathJax.Hub.Queue(["Typeset", window.MathJax.Hub]);
+      }, 0);
+    }
+  }, [latexExpression]);
+
+  useEffect(() => {
+    if (result) {
+      renderLatexToCavas(result.expression, result.answer);
+    }
+  }, [result]);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -42,15 +62,45 @@ export default function Home() {
         ctx.lineWidth = 3; // brush size
       }
     }
+
+    const script = document.createElement("script");
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/mathjax/4.0.0-beta.7/tex-mml-chtml.js";
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.MathJax.Hub.Config({
+        tex2jax: {
+          inlineMath: [
+            ["$", "$"],
+            ["\\(", "\\)"],
+          ],
+        },
+      });
+    };
+
+    return () => {
+      document.head.removeChild(script);
+    };
   }, []);
+
+  const renderLatexToCavas = (expression: string, answer: string) => {
+    const latex = `${expression} = ${answer}`;
+    setLatexExpression([...latexExpression, latex]);
+
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      }
+    }
+  };
 
   const sendData = async () => {
     const canvas = canvasRef.current;
     if (canvas) {
-      // console.log(
-      //   "sending data ...",
-      //   `${import.meta.env.VITE_API_URL}/calculate`
-      // );
       const response = await axios({
         method: "post",
         url: `${import.meta.env.VITE_API_URL}/calculate`,
@@ -60,7 +110,44 @@ export default function Home() {
         },
       });
       const resp = await response.data;
-      console.log("Response:", resp);
+      resp.data.forEach((data: Response) => {
+        if (data.assign == true) {
+          setDictOfVars({
+            ...dictOfVars,
+            [data.expr]: data.result,
+          });
+        }
+      });
+
+      const ctx = canvas.getContext("2d");
+      const imageData = ctx!.getImageData(0, 0, canvas.width, canvas.height);
+      let minX = canvas.width,
+        minY = canvas.height,
+        maxX = 0,
+        maxY = 0;
+
+      for (let y = 0; y < canvas.height; y++) {
+        for (let x = 0; x < canvas.width; x++) {
+          if (imageData.data[(y * canvas.width + x) * 4 + 3] > 0) {
+            if (x < minX) minX = x;
+            if (x > maxX) maxX = x;
+            if (y < minY) minY = y;
+            if (y > maxX) maxY = y;
+          }
+        }
+      }
+      const centerX = (minX + maxX) / 2;
+      const centerY = (minY + maxY) / 2;
+
+      setLatexPosition({ x: centerX, y: centerY });
+      resp.data.forEach((data: Response) => {
+        setTimeout(() => {
+          setResult({
+            expression: data.expr,
+            answer: data.result,
+          });
+        });
+      }, 200);
     }
   };
 
@@ -147,6 +234,19 @@ export default function Home() {
         onMouseUp={stopDrawing}
         onMouseMove={draw}
       />
+
+      {latexExpression &&
+        latexExpression.map((latex, index) => (
+          <Draggable
+            key={index}
+            defaultPosition={latexPosition}
+            onStop={(_e, data) => setLatexPosition({ x: data.x, y: data.y })}
+          >
+            <div className="absolute text-white">
+              <div className="latext-content">{latex}</div>
+            </div>
+          </Draggable>
+        ))}
     </>
   );
 }
