073fe01c0c5b89442be944f5a1ea03209458d403
krmahi <mahesh.kr.2277@gmail.com>
2025-04-21 19:24:17 +0530

    modify color swatches

--- constants.ts
+++ constants.ts
@@ -1,5 +1,6 @@
 const SWATCHES = [
   "#FFFFFF", // White
+  "#676767", // Grey
   "#FF0000", // Red
   "#00FF00", // Lime
   "#0000FF", // Blue
@@ -7,12 +8,11 @@ const SWATCHES = [
   "#FF00FF", // Fuchsia
   "#00FFFF", // Aqua
   "#FFA500", // Orange
-  "#800080", // Purple
-  "#A52A2A", // Brown
+  "#5C4033", // Brown
   "#FFC0CB", // Pink
-  "#000080", // Navy
   "#008080", // Teal
   "#808000", // Olive
+  "#7900ff", // purple
 ];
 
 export { SWATCHES };
